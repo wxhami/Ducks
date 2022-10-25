c3943551a8a73aa456d5ba35a74e434307b420e2
wxhami <d18102002@mail.ru>
2022-10-25 19:22:06 +0300

    add Duck

---
+++ Ducks/Duck.cs
@@ -0,0 +1,83 @@
+﻿namespace Ducks;
+
+public class Duck
+{
+    public double Tonus;
+    public double Sleeping;
+    public int Age;
+    public bool StatusSuper;
+    public double Weight;
+    public string Name;
+
+    public bool IsAlive => CheckAlive();
+        
+    public Duck(string name)
+    {
+        Name = name;
+        Tonus = 0;
+        Sleeping = 0;
+        Age = 0;
+        Weight = 0;
+        
+    }
+
+    public void ShowDuck()
+    {
+        Console.WriteLine($"Name: {Name} \nSleeping: {Sleeping} \nWeight: {Weight} \nTonus: {Tonus} \nAge: {Age}");
+    }
+
+    public void Eat()
+    {
+        Weight += 0.2;
+        Tonus += 1;
+        CheckLackOfSleep();
+    }
+
+    public void Sleep()
+    {
+        Tonus++;
+        CheckLackOfSleep();
+    }
+
+    public void Training()
+    {
+        Tonus++;
+        Sleeping -= 1;
+        Weight -= 0.1;
+        CheckLackOfSleep();
+    }
+
+    public void Swimming()
+    {
+        Tonus++;
+        Sleeping++;
+        CheckLackOfSleep();
+    }
+
+    public void Competicion()
+    {
+        Tonus -= 2;
+        Sleeping -= 2;
+        Weight -= 0.2;
+    }
+
+    private void CheckLackOfSleep()
+    {
+        if (Sleeping < 0)
+        {
+            Tonus -= 0.5;
+        }
+    }
+
+    private bool CheckAlive()
+    {
+        if (Age >= 10 || Weight < 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+    
+    
+}
